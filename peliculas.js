4d2cc301c35b2156eb478e78e16b9d1119e78941
Cardumen <cardumen@luisvilches.local>
2018-03-01 17:07:08 -0300

    add info pelis

--- peliculas.js
+++ peliculas.js
@@ -5,6 +5,10 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/4/4c/Panteranegraposter.jpg/revision/latest/scale-to-width-down/350?cb=20171216194018&path-prefix=es",
         description:"Pantera Negra es una película de 2018 dirigida por Ryan Coogler y protagonizada por Chadwick Boseman, Michael B. Jordan, Lupita Nyong'o, Danai Gurira, Martin Freeman, Daniel Kaluuya, Angela Bassett, Forest Whitaker y Andy Serkis. Es la primera película en solitario de la Pantera Negra dentro del Universo Cinematográfico de Marvel.",
+        trailer:"https://www.youtube.com/watch?v=fsT5SyBLlIg",
+        pais: "Estados Unidos",
+        director: "Ryan Coogler",
+        productora: "Marvel Studios / Walt Disney Pictures",
 
     },
     {
@@ -13,7 +17,10 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/6/62/Avengers_Infiny_War_Poster_Teaser_Latino_JPosters.jpg/revision/latest/scale-to-width-down/338?cb=20171129174702&path-prefix=es",
         description:"Avengers: Infinity War es una próxima película estadounidense de 2018 basada en el equipo de superhéroes de Marvel Comics Los Vengadores, producida por Marvel Studios y distribuida por Walt Disney Studios Motion Pictures.",
-        
+        trailer:"https://www.youtube.com/watch?v=PhUMUr1q22c",
+        pais: "Estados Unidos",
+        director: "Anthony Russo,  Joe Russo",
+        productora: "Marvel Studios"
     },
     {
         id:3,
@@ -21,7 +28,10 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/a/a9/Pacific-rim-uprising-poster.jpg/revision/latest/scale-to-width-down/316?cb=20171006171946&path-prefix=es",
         description:"Titanes del Pacífico: La insurrección es una película de 2018 dirigida por Steven S. DeKnight. Es la secuela de la película de 2013, Titanes del pacífico.",
-        
+        trailer:"https://www.youtube.com/watch?v=nL0BX30LftI",
+        pais: "Estados Unidos",
+        director: "Steven S. DeKnight",
+        productora: "Universal Pictures / Double Dare You (DDY) / Legendary Entertainment / Perfect World Pictures",
     },
     {
         id:4,
@@ -29,7 +39,10 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/f/f3/Un_viaje_en_el_tiempo.jpg/revision/latest/scale-to-width-down/350?cb=20171117193446&path-prefix=es",
         description:"Un viaje en el tiempo es una película de fantasía de 2018 dirigida por Ava DuVernay y protagonizada por Oprah Winfrey, Reese Witherspoon, Mindy Kaling, Storm Reid, Zach Galifianakis y Chris Pine. Basada en la novela Una Arruga en el Tiempo de 1963 de Madeleine L'Engle.",
-        
+        trailer:"https://www.youtube.com/watch?v=dEHloHYJldA",
+        pais: "Estados Unidos",
+        director: "Ava DuVernay",
+        productora: "Walt Disney Pictures / Whitaker Entertainment",
     },
     {
         id:5,
@@ -37,7 +50,11 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/0/00/Afiche_Nuevos_noche.jpg/revision/latest/scale-to-width-down/350?cb=20180207172750&path-prefix=es",
         description:"Noche de juegos es una película dirigida por John Francis Daley y Jonathan Goldstein. Es protagonizada por Jason Bateman y Rachel McAdams.",
-        
+        trailer:"https://www.youtube.com/watch?v=wSJjLxxg_Ao",
+        pais: "Estados Unidos",
+        director: "John Francis Daley,  Jonathan Goldstein",
+        productora: "Warner Bros. / Aggregate Films / Davis Entertainment / New Line Cinema",
+
     },
     {
         id:6,
@@ -45,14 +62,21 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/3/3e/Ready-player-one_comienza_el_juego_poster_latino_final_JPosters.jpg/revision/latest/scale-to-width-down/337?cb=20180215231922&path-prefix=es",
         description:"Ready Player One: Comienza el juego es una próxima película estadounidense de acción y ciencia ficción producida y dirigida por Steven Spielberg y escrita por Zak Penn y Ernest Cline, basada en la novela del mismo nombre de Cline",
-        
+        trailer:"https://www.youtube.com/watch?v=kiD1nBpr8fI",
+        pais: "Estados Unidos",
+        director: "Steven Spielberg",
+        productora: "Warner Bros. Entertainment / Amblin Entertainment / De Line Pictures / Village Roadshow Pictures / Reliance Entertainment",
     },
-    {
+    {   id:14,
         title: "Alita: Ángel de combate",
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/8/83/Alita_Battle_Angel_Poster.png/revision/latest/scale-to-width-down/350?cb=20171208210132&path-prefix=es",
         description:"Alita: Ángel de combate es una película de 2018 dirigida por Robert Rodriguez y producida por James Cameron. Protagonizada por Rosa Salazar, Christoph Waltz y Jennifer Connelly. Está basada en el popular manga del mismo nombre de Yukito Kishiro.",
-        
+        trailer:"https://www.youtube.com/watch?v=TVqSolHUusY",
+        pais: "Estados Unidos",
+        director: "Robert Rodriguez",
+        productora: "20th Century-Fox Film Corporation / Lightstorm Entertainment / Troublemaker Studios",
+
     },
     {
         id:7,
@@ -60,7 +84,11 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/e/e3/MI_6_Poster.png/revision/latest?cb=20180205033155&path-prefix=es",
         description:"Misión: Imposible – Repercusión es una película de 2018, dirigida por Christopher McQuarrie y protagonizada por Tom Cruise. Es la sexta entrega basada en la popular serie de televisión creada por Bruce Geller.",
-        
+        trailer:"https://www.youtube.com/watch?v=dk3M_NVinX0",
+        pais: "Estados Unidos",
+        director: "Christopher McQuarrie",
+        productora: "Paramount Pictures",
+
     },
     {
         id:8,
@@ -68,7 +96,11 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/8/84/22802662_154128591990088_323763117332889600_n.jpg/revision/latest/scale-to-width-down/338?cb=20180130230650&path-prefix=es",
         description:"Operación Red Sparrow es un próximo thriller de acción dirigido por Francis Lawrence, protagonizado por Jennifer Lawrence, Joel Edgerton y Jeremy Irons.",
-        
+        trailer:"https://www.youtube.com/watch?v=-gVUOTivb8o",
+        pais: "Estados Unidos",
+        director: "Francis Lawrence",
+        productora: "Chernin Entertainment / Film Rites / Soundtrack New York. Distribuida por Twentieth Century Fox Film Corporation",
+
     },
     {
         id:9,
@@ -76,7 +108,10 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/6/6f/Jurassic-world-reino-caido.jpg/revision/latest/scale-to-width-down/339?cb=20171207222959&path-prefix=es",
         description:"Jurassic World: El reino caído es una próxima película de 2018 escrita por Colin Trevorrow, dirigida por J.A. Bayona y protagonizada por Chris Pratt, Bryce Dallas Howard, B.D. Wong, James Cromwell, Ted Levine, Justice Smith, Geraldine Chaplin, Daniella Pineda, Toby Jones, Rafe Spall y Jeff Goldblum. Es la secuela de la película Jurassic World: Mundo jurásico.",
-        
+        trailer:"https://www.youtube.com/watch?v=nU99UBjYrmY",
+        pais: "Estados Unidos",
+        director: "J.A. Bayona",
+        productora: "Amblin Entertainment / Universal Pictures",
     },
     {
         id:10,
@@ -84,7 +119,10 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/7/77/Yo_Soy_Simon_Poster.jpg/revision/latest/scale-to-width-down/350?cb=20180130164024&path-prefix=es",
         description:"Yo soy Simón es una película de comedia dramática dirigida por Greg Berlanti y basada en la novela 'Yo Simon, Homo sapiens' de Becky Albertalli. La película está protagonizada por Nick Robinson y Katherine Langford",
-        
+        trailer:"https://www.youtube.com/watch?v=sbZmp6UXOEI",
+        pais: "Estados Unidos",
+        director: "Greg Berlanti",
+        productora: "Fox 2000 Pictures / Temple Hill Entertainment",
     },
     {
         id:11,
@@ -92,7 +130,10 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/f/f6/El_Pasajero_Poster_Latino_JPosters.jpg/revision/latest/scale-to-width-down/350?cb=20180116210636&path-prefix=es",
         description:"El pasajero es una película del 2018 dirigida por Jaume Collet-Serra y protagonizada por Liam Neeson.",
-        
+        trailer:"https://www.youtube.com/watch?v=-SiUMdH8bTg",
+        pais: "Estados Unidos",
+        director: "Jaume Collet-Serra",
+        productora: "Coproducción Estados Unidos-Reino Unido; Lionsgate / StudioCanal / Nvizage / Ombra Films",
     },
     {
         id:12,
@@ -100,7 +141,10 @@ module.exports = [
         year: 2018,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV3b4Ctlm5NxkdwTnDBEIPYmJybeCgcaIEvUajxPr-x2lGTuV9",
         description:"Máquinas mortales es una película del 2018 basada en la novela de Philip Reeve, dirigida por Christian Rivers y escrita y producida por Peter Jackson.",
-        
+        trailer:"https://www.youtube.com/watch?v=kiK3KLmoMO0",
+        pais: "Nueva Zelanda",
+        director: "Christian Rivers",
+        productora: "Coproducción Nueva Zelanda-Estados Unidos; Universal Pictures / Media Rights Capital / Scholastic Productions / Silvertongue Films / Wingnut Films",
     },
     {
         id:13,
@@ -108,6 +152,9 @@ module.exports = [
         year: 2018,
         img: "https://vignette.wikia.nocookie.net/doblaje/images/0/0a/Step-Sisters-poster.jpg/revision/latest/scale-to-width-down/337?cb=20180120022514&path-prefix=es",
         description:"Step Sisters es una película de Netflix del 2018 protagonizada por Megalyn Echikunwoke.",
-        
+        trailer:"https://www.youtube.com/watch?v=qMXO6Up6pvE",
+        pais: "Estados Unidos",
+        director: "Charles Stone III",
+        productora: "Los Angeles Media Fund (LAMF). Distribuida por Netflix",
     }
 ]
